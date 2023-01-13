b40d889844f66a21a0302ad917e9091ab9445349
marloCoffey <60888841+marloCoffey@users.noreply.github.com>
2023-01-13 16:32:55 +0000

    update site, dark theme, update date picker

--- js/datetimepicker.js
+++ js/datetimepicker.js
@@ -41,7 +41,7 @@
 		formatTime:	'H:i',
 		formatDate:	'Y/m/d',
 
-		startDate:	false, // new Date(), '1986/12/08', '-1970/01/05','-1970/01/05',
+		startDate:	new Date(), // new Date(), '1986/12/08', '-1970/01/05','-1970/01/05',
 		step: 60,
 		monthChangeSpinner: true,
 
@@ -1683,6 +1683,9 @@ $('#default_datetimepicker').datetimepicker({
 	timepickerScrollbar:false
 });
 
+$('#datetimepicker14').datetimepicker({
+	format: 'DD-MM-YYYY LT'
+});
 $('#datetimepicker10').datetimepicker({
 	step:5,
 	inline:true
